a83295eb5af286d8eae7553c09c9437a52a936d2
nonitut <nona.blogg@gmail.com>
2022-02-14 22:49:54 +0300

    new1

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,1 +1,16 @@
 //write here your js
+$(document).ready(function(){
+    $(".move-area").mousemove(function(event) {
+      let eye = $(".eye");
+      let x = (eye.offset().left) + (eye.width() / 2);
+      let y = (eye.offset().top) + (eye.height() / 2);
+      let rad = Math.atan2(event.pageX - x, event.pageY - y);
+      let rot = (rad * (180 / Math.PI) * -1) + 180;
+      eye.css({
+        '-webkit-transform': 'rotate(' + rot + 'deg)',
+        '-moz-transform': 'rotate(' + rot + 'deg)',
+        '-ms-transform': 'rotate(' + rot + 'deg)',
+        'transform': 'rotate(' + rot + 'deg)'
+      });
+    });
+  })
